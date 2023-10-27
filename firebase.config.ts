959e141e94509b047ae076b4ae50a45bb2bab6d2
Victor Martins <martinsvictor287@gmail.com>
2023-10-27 07:43:14 +0300

    fix firebase config

--- firebase.config.ts
+++ firebase.config.ts
@@ -5,12 +5,12 @@ import { Auth, getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-	apiKey: 'YOUR_API_KEY',
-	authDomain: 'YOUR_AUTH_DOMAIN',
-	projectId: 'YOUR_PROJECT_ID',
-	storageBucket: 'YOUR_STORAGE_BUCKET',
-	messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-	appId: 'YOUR_APP_ID',
+	apiKey: 'AIzaSyBNsXGA0T3gWFGS69f1YgtXMx83tiLp22A',
+	authDomain: 'auth-nc-47ccc.firebaseapp.com',
+	projectId: 'auth-nc-47ccc',
+	storageBucket: 'auth-nc-47ccc.appspot.com',
+	messagingSenderId: '221915830112',
+	appId: '1:221915830112:web:53fc43831484aef76bb005',
 };
 
 const app = initializeApp(firebaseConfig);
