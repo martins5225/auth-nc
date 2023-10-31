99ddbd5f622de90506b088b27a097b0b4ad67db5
Victor Martins <martinsvictor287@gmail.com>
2023-10-31 19:33:20 +0300

    fix profile routing

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
 		element: <LoginContainer />,
 	},
 	{
-		path: '/profile/:userId',
+		path: '/profile',
 		element: (
 			<AuthRoute>
 				<ProfileContainer />
